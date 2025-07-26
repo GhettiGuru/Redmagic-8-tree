b34a63899f11f0b3ed40db2cf1afb953b116ed47
Resinous <i.want.it.to.turn.black@gmail.com>
2025-07-26 11:46:15 -0700

    Add files via upload

---
+++ device/nubia/nx729j/vendorsetup.sh
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_LUNCH_CHOICES=(
+    twrp_nx729j-user
+    twrp_nx729j-userdebug
+    twrp_nx729j-eng
+)
