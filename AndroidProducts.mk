53b8921bdc25d6b63a0c8a17e8403f0eec2fb7a7
Resinous <i.want.it.to.turn.black@gmail.com>
2025-09-09 16:37:21 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_NX729J.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_nx729j.mk
 
-COMMON_LUNCH_CHOICES := twrp_Nx729j-eng
+COMMON_LUNCH_CHOICES := twrp_nx729j-eng
