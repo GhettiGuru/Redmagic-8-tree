857b85c188d169c1599eb5c697dc947e10affeb2
Resinous <i.want.it.to.turn.black@gmail.com>
2025-09-09 16:33:54 -0500

    Update twrp_NX729J.mk

--- twrp_NX729J.mk
+++ twrp_NX729J.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/nubia/nx729j
+DEVICE_PATH := device/nubia/NX729J
 
 # Inherit from device.mk configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
